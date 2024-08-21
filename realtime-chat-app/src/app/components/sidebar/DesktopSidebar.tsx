5c63f36d64d1fd53530c0a15319c6f052cbd68c8
shadeshsaha <shadesh15-8599@diu.edu.bd>
2024-08-21 19:02:19 +0600

    Added Avatar and Completed Sidebar, Navigation and Layout

--- realtime-chat-app/src/app/components/sidebar/DesktopSidebar.tsx
+++ realtime-chat-app/src/app/components/sidebar/DesktopSidebar.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import useRoutes from "@/app/hooks/useRoutes";
+import { User } from "@prisma/client";
 import { useState } from "react";
+import Avatar from "../Avatar";
 import DesktopItem from "./DesktopItem";
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+  currentUser: User;
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
+
+  console.log("currentUser: ", currentUser);
 
   return (
     <div
@@ -55,6 +63,27 @@ const DesktopSidebar = () => {
             />
           ))}
         </ul>
+      </nav>
+
+      <nav
+        className="
+        mt-4
+        flex
+        flex-col
+        justify-between
+        items-center
+        "
+      >
+        <div
+          onClick={() => setIsOpen(true)}
+          className="
+          cursor-pointer
+          hover:opacity-75
+          transition
+          "
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   );
